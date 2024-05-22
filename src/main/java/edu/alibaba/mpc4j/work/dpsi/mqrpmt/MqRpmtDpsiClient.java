487e168aa8024f70b4944d78827705c6c42945e4
FlyFF <wyfs4321@163.com>
2024-05-22 15:10:58 +0800

    Add files via upload

---
+++ src/main/java/edu/alibaba/mpc4j/work/dpsi/mqrpmt/MqRpmtDpsiClient.java
@@ -0,0 +1,167 @@
+package edu.alibaba.mpc4j.work.dpsi.mqrpmt;
+
+import edu.alibaba.mpc4j.common.rpc.*;
+import edu.alibaba.mpc4j.common.rpc.utils.DataPacket;
+import edu.alibaba.mpc4j.common.rpc.utils.DataPacketHeader;
+import edu.alibaba.mpc4j.common.tool.CommonConstants;
+import edu.alibaba.mpc4j.common.tool.bitvector.BitVector;
+import edu.alibaba.mpc4j.common.tool.bitvector.BitVectorFactory;
+import edu.alibaba.mpc4j.common.tool.crypto.hash.Hash;
+import edu.alibaba.mpc4j.common.tool.crypto.hash.HashFactory;
+import edu.alibaba.mpc4j.common.tool.utils.IntUtils;
+import edu.alibaba.mpc4j.common.tool.utils.ObjectUtils;
+import edu.alibaba.mpc4j.dp.cdp.numeric.integral.bound.BoundIntegralCdp;
+import edu.alibaba.mpc4j.dp.cdp.numeric.integral.bound.BoundIntegralCdpFactory;
+import edu.alibaba.mpc4j.work.dpsi.AbstractDpsiClient;
+import edu.alibaba.mpc4j.work.dpsi.mqrpmt.MqRpmtDpsiPtoDesc.PtoStep;
+import edu.alibaba.mpc4j.s2pc.opf.mqrpmt.MqRpmtFactory;
+import edu.alibaba.mpc4j.s2pc.opf.mqrpmt.MqRpmtServer;
+
+import java.nio.ByteBuffer;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+
+public class MqRpmtDpsiClient<T> extends AbstractDpsiClient<T> {
+    /**
+     * mqRPMT
+     */
+    private final MqRpmtServer mqRpmtServer;
+    /**
+     * max PSI-CA dummy size
+     */
+    private final int maxPsicaDummySize;
+    /**
+     * PSI-CA CDP
+     */
+    private final BoundIntegralCdp psicaCdp;
+    /**
+     * max PSI-DA dummy size
+     */
+    private final int maxPsdcaDummySize;
+    /**
+     * PSD-CA CDP
+     */
+    private final BoundIntegralCdp psdcaCdp;
+    /**
+     * element hash
+     */
+    private final Hash elementHash;
+
+    public MqRpmtDpsiClient(Rpc clientRpc, Party serverParty, MqRpmtDpsiConfig config) {
+        super(MqRpmtDpsiPtoDesc.getInstance(), clientRpc, serverParty, config);
+        mqRpmtServer = MqRpmtFactory.createServer(clientRpc, serverParty, config.getMqRpmtConfig());
+        addSubPto(mqRpmtServer);
+        maxPsicaDummySize = config.getMaxPsicaDummySize();
+        psicaCdp = BoundIntegralCdpFactory.createInstance(config.getPsicaCdpConfig());
+        maxPsdcaDummySize = config.getMaxPsdcaDummySize();
+        psdcaCdp = BoundIntegralCdpFactory.createInstance(config.getPsdcaCdpConfig());
+        elementHash = HashFactory.createInstance(envType, CommonConstants.BLOCK_BYTE_LENGTH);
+    }
+
+    @Override
+    public void init(int maxClientElementSize, int maxServerElementSize) throws MpcAbortException {
+        setInitInput(maxClientElementSize, maxServerElementSize);
+        logPhaseInfo(PtoState.INIT_BEGIN);
+
+        stopWatch.start();
+        mqRpmtServer.init(
+            maxClientElementSize + maxPsicaDummySize + maxPsdcaDummySize,
+            maxServerElementSize + maxPsicaDummySize
+        );
+        stopWatch.stop();
+        long initTime = stopWatch.getTime(TimeUnit.MILLISECONDS);
+        stopWatch.reset();
+        logStepInfo(PtoState.INIT_STEP, 1, 1, initTime);
+
+        logPhaseInfo(PtoState.INIT_END);
+    }
+
+    @Override
+    public Set<T> psi(Set<T> clientElementSet, int serverElementSize) throws MpcAbortException {
+        setPtoInput(clientElementSet, serverElementSize);
+        logPhaseInfo(PtoState.PTO_BEGIN);
+
+        stopWatch.start();
+        // client adds dummy elements
+        Map<ByteBuffer, T> dummyClientElementMap = createDummyClientElementSet();
+        int dummyClientElementSize = dummyClientElementMap.size();
+        List<byte[]> clientDummyElementSizePayload = Collections.singletonList(
+            IntUtils.intToByteArray(dummyClientElementSize)
+        );
+        DataPacketHeader clientDummyElementSizeHeader = new DataPacketHeader(
+            encodeTaskId, getPtoDesc().getPtoId(), PtoStep.CLIENT_SEND_ELEMENTS_SIZE.ordinal(), extraInfo,
+            ownParty().getPartyId(), otherParty().getPartyId()
+        );
+        rpc.send(DataPacket.fromByteArrayList(clientDummyElementSizeHeader, clientDummyElementSizePayload));
+        stopWatch.stop();
+        long clientDummyElementTime = stopWatch.getTime(TimeUnit.MILLISECONDS);
+        stopWatch.reset();
+        logStepInfo(PtoState.PTO_STEP, 1, 3, clientDummyElementTime);
+
+        stopWatch.start();
+        Set<ByteBuffer> dummyClientElementSet = dummyClientElementMap.keySet();
+        ByteBuffer[] clientVector = mqRpmtServer.mqRpmt(
+            dummyClientElementSet, serverElementSize + maxPsicaDummySize
+        );
+        stopWatch.stop();
+        long mqRpmtTime = stopWatch.getTime(TimeUnit.MILLISECONDS);
+        stopWatch.reset();
+        logStepInfo(PtoState.PTO_STEP, 2, 3, mqRpmtTime);
+
+        DataPacketHeader randomizedServerVectorHeader = new DataPacketHeader(
+            encodeTaskId, getPtoDesc().getPtoId(), PtoStep.SERVER_SEND_RANDOMIZED_VECTOR.ordinal(), extraInfo,
+            otherParty().getPartyId(), ownParty().getPartyId()
+        );
+        List<byte[]> randomizedServerVectorPayload = rpc.receive(randomizedServerVectorHeader).getPayload();
+
+        stopWatch.start();
+        MpcAbortPreconditions.checkArgument(randomizedServerVectorPayload.size() == 1);
+        int bitNum = clientVector.length;
+        BitVector serverContainVector = BitVectorFactory.create(bitNum, randomizedServerVectorPayload.get(0));
+        Set<T> intersection = IntStream.range(0, bitNum)
+            .mapToObj(i -> {
+                if (serverContainVector.get(i)) {
+                    return dummyClientElementMap.get(clientVector[i]);
+                } else {
+                    return null;
+                }
+            })
+            .filter(Objects::nonNull)
+            .collect(Collectors.toSet());
+        stopWatch.stop();
+        long intersectionTime = stopWatch.getTime(TimeUnit.MILLISECONDS);
+        stopWatch.reset();
+        logStepInfo(PtoState.PTO_STEP, 3, 3, intersectionTime);
+
+        logPhaseInfo(PtoState.PTO_END);
+        return intersection;
+    }
+
+    public Map<ByteBuffer, T> createDummyClientElementSet() {
+        int psicaDummySize = psicaCdp.randomize(0);
+        int psdcaDummySize = psdcaCdp.randomize(0);
+        Map<ByteBuffer, T> dummyClientElementMap = new HashMap<>(clientElementSize + psicaDummySize + psdcaDummySize);
+        clientElementArrayList.forEach(element -> {
+            byte[] hashElement = elementHash.digestToBytes(ObjectUtils.objectToByteArray(element));
+            hashElement[0] = (byte) (hashElement[0] & (byte) 0x7F);
+            dummyClientElementMap.put(ByteBuffer.wrap(hashElement), element);
+        });
+        // add dummy points for PSI-CA
+        IntStream.range(0, psicaDummySize).forEach(index -> {
+            byte[] dummyElement = IntUtils.nonNegIntToFixedByteArray(index, CommonConstants.BLOCK_BYTE_LENGTH);
+            dummyElement[0] = (byte) (dummyElement[0] | (byte) 0x80);
+            dummyClientElementMap.put(ByteBuffer.wrap(dummyElement), null);
+        });
+        // add dummy points for PSD-CA
+        IntStream.range(0, psdcaDummySize).forEach(index -> {
+            byte[] dummyElement = IntUtils.nonNegIntToFixedByteArray(index, CommonConstants.BLOCK_BYTE_LENGTH);
+            dummyElement[0] = (byte) (dummyElement[0] | (byte) 0x84);
+            dummyClientElementMap.put(ByteBuffer.wrap(dummyElement), null);
+        });
+
+        return dummyClientElementMap;
+    }
+}
---
+++ src/test/java/edu/alibaba/mpc4j/work/dpsi/DpPsiParameterTest.java
@@ -0,0 +1,26 @@
+package edu.alibaba.mpc4j.work.dpsi;
+
+import edu.alibaba.mpc4j.work.dpsi.mqrpmt.MqRpmtDpsiConfig;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@Ignore
+public class DpPsiParameterTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DpPsiParameterTest.class);
+
+    @Test
+    public void testMqRpmtMaxDummySize() {
+        for (int exp = -10; exp <= 10; exp++) {
+            double epsilon = Math.pow(2, exp);
+            MqRpmtDpsiConfig config = new MqRpmtDpsiConfig.Builder(epsilon, epsilon / 2, epsilon / 2).build();
+            LOGGER.info(
+                "ε = 2^{}, PSI-CA-R = {}, PSD-CA-R = {}",
+                exp - 1, config.getMaxPsicaDummySize(), config.getMaxPsdcaDummySize()
+            );
+
+        }
+    }
+}
